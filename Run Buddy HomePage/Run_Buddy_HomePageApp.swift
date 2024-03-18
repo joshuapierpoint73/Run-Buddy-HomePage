4d35bc1c20c0178f806af0a70284c55f12c8410d
Joshua <joshuapierpoint73@gmail.com>
2024-03-18 13:45:50 +0000

    Initial Commit

---
+++ Run Buddy HomePage/Run_Buddy_HomePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_Buddy_HomePageApp.swift
+//  Run Buddy HomePage
+//
+//  Created by Pierpoint, Josh on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Run_Buddy_HomePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
